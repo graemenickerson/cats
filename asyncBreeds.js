74fcb7782be608467f42600adf1cf0ecddcc2150
graemenickerson <graeme.nickerson@gmail.com>
2019-09-24 23:34:46 +0000

    Updates file in project for linting

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -8,9 +8,9 @@ const breedDetailsFromFile = function(breed, callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
 
-    console.log('Callback: I have the data!')
-    if (!error) callback(data); 
+    console.log('Callback: I have the data!');
+    if (!error) callback(data);
   });
-}
+};
 
 module.exports = breedDetailsFromFile;
