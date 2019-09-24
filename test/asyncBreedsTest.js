916bc48ccab2bef11b63d5853e316d6a77e7f506
graemenickerson <graeme.nickerson@gmail.com>
2019-09-24 23:27:28 +0000

    Updates asyncBreeds.js to export breedDetailsFromFile. Adds asyncBreedsTest.js to repository

---
+++ test/asyncBreedsTest.js
@@ -0,0 +1,12 @@
+// asyncBreedsTest.js
+// Graeme Nickerson
+// September 24, 2019
+
+const assert = require('chai').assert;
+const breedDetailsFromFile = require('../asyncBreeds');
+
+describe('#breedDetailsFromFile', () => {
+  it('provides, via callback, breed details for the Bombay breed', () => {
+    console.log(breedDetailsFromFile);
+  });
+});
